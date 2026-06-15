e2e03f8a04f85a9d159a60ae00e7b5fd883c1c77
Youssif Al-Halawche <yhalawche@gmail.com>
2026-06-15 03:12:49 -0600

    include prebuilt next output for azure

---
+++ .next/server/app/api/feedback/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/feedback/route.js")
+R.c("server/chunks/[root-of-the-server]__0fr6k59._.js")
+R.c("server/chunks/[root-of-the-server]__0wpxw_-._.js")
+R.c("server/chunks/[root-of-the-server]__0r-cy4-._.js")
+R.c("server/chunks/_next-internal_server_app_api_feedback_route_actions_0k5oivr.js")
+R.m(57982)
+module.exports=R.m(57982).exports
---
+++ .next/server/app/api/submit/route.js
@@ -0,0 +1,7 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/submit/route.js")
+R.c("server/chunks/[root-of-the-server]__0f2puib._.js")
+R.c("server/chunks/[root-of-the-server]__0wpxw_-._.js")
+R.c("server/chunks/[root-of-the-server]__0r-cy4-._.js")
+R.c("server/chunks/_next-internal_server_app_api_submit_route_actions_1n22xyy.js")
+R.m(95251)
+module.exports=R.m(95251).exports
